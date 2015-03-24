a01c7a60c8d1fc6e3c10c53faa352af47b2bcbe9
Ivan <ivan.herndon@detroitlabs.com>
2015-03-24 00:58:00 -0400

    The logic for the game of life is working perfectly now, but I am manually going through each iteration. Looking to automate this process

--- life.js
+++ life.js
@@ -20,11 +20,14 @@ function cell (display, isAlive, nextIt){
 	this.display = display;
 	this.isAlive = isAlive;
 	this.nextIt = nextIt;
+	this.changeStatus = function(){
+		return this.isAlive = this.nextIt;
+	}
 	this.checkStatus = function(){
 		if (this.isAlive){
 			this.display = "0";
 		} else {
-			this.displayu = "-";
+			this.display = "-";
 		}
 	}
 }
@@ -33,16 +36,7 @@ for (var i = 0; i < 200; i++){
 	game[i] = new cell("-", false, false);
 }
 
-rowOne = createRow(0);
-rowTwo = createRow(1);
-rowThree = createRow(2);
-rowFour = createRow(3);
-rowFive = createRow(4);
-rowSix = createRow(5);
-rowSeven = createRow(6);
-rowEight = createRow(7);
-rowNine = createRow(8);
-rowTen = createRow(9);
+
 
 function printBoard(){
 	console.log(rowOne.join(''));
@@ -67,27 +61,52 @@ function createRow(rowNum){
 	return tempArray
 }
 
-console.log(rowOne.join(''));
-
-var tempArray1 = [];
-
-for (cell in game){
-	var tempArray1 = [];
-	var alive = 0;
-	tempArray1.push(game[cell + 20], game[cell-20], game[cell + 1], game[cell - 1], game[cell - 19], game[cell + 19], game[cell - 21], game[cell + 21]);
-	console.log(tempArray1);
-	tempArray1.forEach(function(item){
-		if (tempArray1[item].isAlive === true)
-			alive += 1;
-	})
-	if(game[cell].isAlive === true && (alive < 2 || alive > 3)) {
-	game[cell].isAlive = false;
-	} else if (game[cell].isAlive == false && alive === 3) {
-		game[cell].isAlive = true;
+function pusher(num, array){
+	if (game[Number(cell) + num] !== undefined){
+		array.push(game[Number(cell)+num]);
+	}
+	if (game[Number(cell) - num] !== undefined){
+		array.push(game[Number(cell) - num]);
 	}
 }
 
-printBoard();
+
+
+
+function iteration(){
+	// var tempArray1 = [];
+	for (cell in game){
+		var tempArray1 = [];
+		var alive = 0;
+		pusher(20, tempArray1);
+		pusher(1, tempArray1);
+		pusher(19, tempArray1);
+		pusher(21, tempArray1);
+		// tempArray1.push(game[cell + 20], game[cell-20], game[cell + 1], game[cell - 1], game[cell - 19], game[cell + 19], game[cell - 21], game[cell + 21]);
+		// console.log(tempArray1);
+		tempArray1.forEach(function(item){
+			if (item.isAlive === true)
+				alive += 1;
+			});
+		if(game[cell].isAlive === true && (alive < 2 || alive > 3)) {
+			game[cell].nextIt = false;
+		} else if (game[cell].isAlive === false && alive === 3) {
+			game[cell].nextIt = true;
+		} else if (game[cell].isAlive === false){
+			game[cell].nextIt = false;
+		} else {
+			game[cell].nextIt = true;
+		}
+		// console.log(alive);
+	}
+}
+
+game[70].isAlive = true;
+game[71].isAlive = true;
+game[72].isAlive = true;
+game[89].isAlive = true;
+game[90].isAlive = true;
+game[91].isAlive = true;
 
 for (cell in game){
 	game[cell].checkStatus();
@@ -104,7 +123,83 @@ rowEight = createRow(7);
 rowNine = createRow(8);
 rowTen = createRow(9);
 
+
+
 printBoard();
+
+iteration();
+
+for (cell in game){
+	game[cell].changeStatus();
+}
+
+for (cell in game){
+	game[cell].checkStatus();
+}
+
+rowOne = createRow(0);
+rowTwo = createRow(1);
+rowThree = createRow(2);
+rowFour = createRow(3);
+rowFive = createRow(4);
+rowSix = createRow(5);
+rowSeven = createRow(6);
+rowEight = createRow(7);
+rowNine = createRow(8);
+rowTen = createRow(9);
+
+console.log("-----------------------------------")
+
+printBoard();
+
+iteration();
+
+for (cell in game){
+	game[cell].changeStatus();
+}
+for (cell in game){
+	game[cell].checkStatus();
+}
+
+rowOne = createRow(0);
+rowTwo = createRow(1);
+rowThree = createRow(2);
+rowFour = createRow(3);
+rowFive = createRow(4);
+rowSix = createRow(5);
+rowSeven = createRow(6);
+rowEight = createRow(7);
+rowNine = createRow(8);
+rowTen = createRow(9);
+
+console.log("-----------------------------------")
+
+printBoard();
+
+iteration();
+
+for (cell in game){
+	game[cell].changeStatus();
+}
+for (cell in game){
+	game[cell].checkStatus();
+}
+
+rowOne = createRow(0);
+rowTwo = createRow(1);
+rowThree = createRow(2);
+rowFour = createRow(3);
+rowFive = createRow(4);
+rowSix = createRow(5);
+rowSeven = createRow(6);
+rowEight = createRow(7);
+rowNine = createRow(8);
+rowTen = createRow(9);
+
+console.log("-----------------------------------")
+
+printBoard();
+
 
 
 
